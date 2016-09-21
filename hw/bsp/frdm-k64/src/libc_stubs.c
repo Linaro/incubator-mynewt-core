bf16d04e0a4a55a471ed4a7ab471ab394554a17b
Michael Scott <michael.scott@linaro.org>
2016-09-21 13:12:05 -0700

    bsp: remove _write and _read from libc_stubs
    These are now implemented in fsl_debug_console code
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>

--- hw/bsp/frdm-k64/src/libc_stubs.c
+++ hw/bsp/frdm-k64/src/libc_stubs.c
@@ -23,10 +23,8 @@ int _close(int fd);
 int _fstat(int fd, void *s);
 void _exit(int s);
 int _kill(int pid, int sig);
-int _write(int fd, void *b, int nb);
 int _isatty(int c);
 int _lseek(int fd, int off, int w);
-int _read(int fd, void *b, int nb);
 int _getpid(void);
 
 int
@@ -55,12 +53,6 @@ _kill(int pid, int sig)
 }
 
 int
-_write(int fd, void *b, int nb)
-{
-    return -1;
-}
-
-int
 _isatty(int c)
 {
     return -1;
@@ -68,12 +60,6 @@ _isatty(int c)
 
 int
 _lseek(int fd, int off, int w)
-{
-    return -1;
-}
-
-int
-_read(int fd, void *b, int nb)
 {
     return -1;
 }
