96015909e13e6abf159fcf80713f4fd628885c63
Michael Scott <michael.scott@linaro.org>
2016-09-21 12:37:51 -0700

    bsp: add NFFS_AREA_MAX definition
    setting this to max 8 areas for now similar to all existing HW setups
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>

--- hw/bsp/frdm-k64/include/bsp/bsp.h
+++ hw/bsp/frdm-k64/include/bsp/bsp.h
@@ -25,6 +25,14 @@ extern "C" {
 
 /* TODO */
 
+/* This defines the maximum NFFS areas (block) are in the BSPs NFS file
+ * system space.  This in conjunction with flash map determines how
+ * many NFS blocks there will be.  A minimum is the number of individually
+ * erasable sectors in the flash area and the maximum is this number. If
+ * your max is less than the number of sectors then the NFFS will combine
+ * multiple sectors into an NFFS area */
+#define NFFS_AREA_MAX    (8)
+
 #ifdef __cplusplus
 }
 #endif
